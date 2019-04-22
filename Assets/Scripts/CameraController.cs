1d529d0bab813a5e9c4a6876d9be2d4c9e524cba
Trooperjack <43067312+Trooperjack@users.noreply.github.com>
2019-04-22 14:40:25 +0100

    Basic First Person Controls Added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+
+    //public variables
+    public float minimumX = -60f;
+    public float maximumX = 60f;
+    public float minimumY = -360f;
+    public float maximumY = 360;
+
+    public float sensitivityX = 15f;
+    public float sensitivityY = 15f;
+
+    public Camera cam;
+    public PlayerController player;
+
+    //private variables
+    float rotationY = 0f;
+    float rotationX = 0f;
+
+    private Vector3 offset;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        offset = cam.transform.position - player.transform.position;
+    }
+
+
+    void Update () {
+        rotationY += Input.GetAxis("Mouse X") * sensitivityY;
+        rotationX += Input.GetAxis("Mouse Y") * sensitivityX;
+
+        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+
+        transform.localEulerAngles = new Vector3(0, rotationY, 0);
+
+        cam.transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+    }
+
+    void LateUpdate()
+    {
+        cam.transform.position = player.transform.position + offset;
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //https://www.mvcode.com/lessons/first-person-camera-and-controller-jamie
+
+    //Public variables
+    public float walkSpeed;
+
+    //Private variables
+    Rigidbody rb;
+    Vector3 moveDirection;
+
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+
+    void Update () {
+        //Get directional input from the user
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+	}
+
+    void FixedUpdate()
+    {
+        //Call the move function
+        Move();
+    }
+
+
+
+    void Move()
+    {
+        Vector3 yVelFix = new Vector3(0, rb.velocity.y, 0);
+        rb.velocity = moveDirection * walkSpeed * Time.deltaTime;
+        rb.velocity += yVelFix;
+    }
+
+
+}
