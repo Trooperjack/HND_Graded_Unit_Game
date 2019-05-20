e02ffbb174aab68f620dd498a59655bee7f84e8c
Trooperjack <43067312+Trooperjack@users.noreply.github.com>
2019-05-20 17:03:17 +0100

    Basic FPS Shooting Added & Working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,25 @@ public class PlayerController : MonoBehaviour {
     public float walkSpeed;
     public Transform gunTip;
 
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    public int startingMagazine = 10;
+    public int maxMagazine = 10;
+    public int currentMagazine;
+    public int startingAmmo = 60;
+    public int maxAmmo = 60;
+    public int currentAmmo;
+    public int bulletsRemaining;
+    public bool isReloading;
+    public bool isEmpty;
+    public bool canFire;
+
     //Private variables
     Rigidbody rb;
     Vector3 moveDirection;
@@ -20,8 +39,15 @@ public class PlayerController : MonoBehaviour {
     LineRenderer bulletLine;
 
     private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
+    private float nextFire;
+    private int shotsFired;
 
     Text messageText;
+    Text shootingText;
+    Text healthText;
+    Text magazineText;
+    Text ammoText;
+    Text reloadText;
 
 
     private void Awake()
@@ -31,15 +57,61 @@ public class PlayerController : MonoBehaviour {
         bulletLine = GetComponent<LineRenderer>();
 
         cam = Camera.main;
+        //cam = GetComponentInParent<Camera>();
 
         messageText = GameObject.Find("Canvas/MessageText").GetComponent<Text>();
+        shootingText = GameObject.Find("Canvas/ShootingText").GetComponent<Text>();
+        healthText = GameObject.Find("Canvas/HealthText").GetComponent<Text>();
+        magazineText = GameObject.Find("Canvas/MagazineText").GetComponent<Text>();
+        ammoText = GameObject.Find("Canvas/AmmoText").GetComponent<Text>();
+        reloadText = GameObject.Find("Canvas/ReloadText").GetComponent<Text>();
+
+        shotsFired = 0;
+        currentHealth = startingHealth;
+        currentMagazine = startingMagazine;
+        currentAmmo = startingAmmo;
     }
 
 
 
     void Update () {
         CheckInteraction();
+        CheckReloadText();
         Shoot();
+        
+        shootingText.text = "" + shotsFired;
+        healthText.text = "" + currentHealth;
+        magazineText.text = "" + currentMagazine;
+        ammoText.text = "/ " + currentAmmo;
+
+        if (currentMagazine != 0 && isReloading == false)
+        {
+            canFire = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && !isReloading && currentMagazine < maxMagazine && currentAmmo > 0)
+        {
+            isReloading = true;
+            StartCoroutine(ReloadDelay());
+        }
+
+        if (currentMagazine < 0)
+        {
+            currentMagazine = 0;
+            canFire = false;
+        }
+        if (currentAmmo < 0)
+        {
+            currentAmmo = 0;
+        }
+        if (currentAmmo > 0)
+        {
+            isEmpty = false;
+        }
+        if (currentAmmo <= 0 && currentMagazine <= 0)
+        {
+            isEmpty = true;
+        }
 
         Vector3 lineOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
         Debug.DrawRay(lineOrigin, cam.transform.forward * 1000f, Color.green);
@@ -59,7 +131,6 @@ public class PlayerController : MonoBehaviour {
         }
 
         moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
-
     }
 
     void FixedUpdate()
@@ -127,28 +198,92 @@ public class PlayerController : MonoBehaviour {
 
     void Shoot()
     {
-        //bulletLine.enabled = false;
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time > nextFire && canFire == true && !isReloading && currentMagazine > 0)
         {
-
-            //bulletLine.enabled = true;
+            shotsFired++;
+            currentMagazine--;
+            nextFire = Time.time + fireRate;
             StartCoroutine(ShotEffect());
             Vector3 rayOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
-            bulletLine.SetPosition(0, gunTip.transform.position);
 
             RaycastHit hit;
-            if (Physics.Raycast(rayOrigin, cam.transform.forward, out hit, 1000))
+            bulletLine.SetPosition(0, gunTip.transform.position);
+
+            if (Physics.Raycast(rayOrigin, cam.transform.forward, out hit, weaponRange))
             {
                 bulletLine.SetPosition(1, hit.point);
+                ShootableBox health = hit.collider.GetComponent<ShootableBox>();
+
+                if (health != null)
+                {
+                    health.Damage(gunDamage);
+                }
+
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * hitForce);
+                }
+
             }
             else
             {
-                bulletLine.SetPosition(1, rayOrigin + (1000f * cam.transform.forward));
+                bulletLine.SetPosition(1, rayOrigin + (cam.transform.forward * weaponRange));
             }
         }
 
     }
+
+
+
+
+
+    void CheckReloadText()
+    {
+
+        reloadText.text = "";
+
+        if(currentMagazine <= 0 && !isReloading && !isEmpty)
+        {
+            reloadText.text = "Press R to Reload Weapon";
+        }
+        else
+        {
+            if (isReloading == true)
+            {
+                reloadText.text = "Reloading...";
+            }
+            else
+            {
+                if (isEmpty == true && !isReloading)
+                {
+                    reloadText.text = "No Ammo";
+                }
+            }
+        }
+
+    }
+
+
+    private IEnumerator ReloadDelay()
+    {
+        yield return new WaitForSeconds(3);
+        if (currentAmmo > maxMagazine - currentMagazine)
+        {
+            bulletsRemaining = maxMagazine - currentMagazine;
+            currentMagazine = maxMagazine;
+            currentAmmo = currentAmmo - bulletsRemaining;
+        }
+        else
+        {
+            bulletsRemaining = maxMagazine - currentMagazine;
+            currentMagazine = currentMagazine + currentAmmo;
+            currentAmmo = 0;
+        }
+        isReloading = false;
+        canFire = true;
+    }
+
 
 
     private IEnumerator ShotEffect()
